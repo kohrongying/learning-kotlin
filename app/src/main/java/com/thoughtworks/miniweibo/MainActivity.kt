5fe2a6c9250509488f0d2ca787bf79ffdc4b8524
rongying <rongying.koh@thoughtworks.com>
2020-06-09 15:27:07 +0800

    Add timeline and detail fragment with nav functions

--- app/src/main/java/com/thoughtworks/miniweibo/MainActivity.kt
+++ app/src/main/java/com/thoughtworks/miniweibo/MainActivity.kt
@@ -2,10 +2,19 @@ package com.thoughtworks.miniweibo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navController = Navigation.findNavController(this, R.id.container)
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = Navigation.findNavController(this, R.id.container)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
---
+++ app/src/main/java/com/thoughtworks/miniweibo/ui/home/HomeTimelineFragment.kt
@@ -0,0 +1,27 @@
+package com.thoughtworks.miniweibo.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import com.thoughtworks.miniweibo.R
+import kotlinx.android.synthetic.main.home_timeline_fragment.*
+
+class HomeTimelineFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.home_timeline_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        button.setOnClickListener { view -> view.findNavController().navigate(R.id.postDetailFragment) }
+    }
+}
