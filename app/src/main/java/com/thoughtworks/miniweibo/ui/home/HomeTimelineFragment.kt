d720f3c548a6f2cde1ac7e3ebc19b6ef205e84b3
rongying <rongying.koh@thoughtworks.com>
2020-06-11 16:00:50 +0800

    Add HomeTimeline ViewModel and use LiveData. Add View Binding to bind layout to fragment

--- app/src/main/java/com/thoughtworks/miniweibo/ui/home/HomeTimelineFragment.kt
+++ app/src/main/java/com/thoughtworks/miniweibo/ui/home/HomeTimelineFragment.kt
@@ -4,28 +4,49 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.findNavController
 import com.thoughtworks.miniweibo.R
-import kotlinx.android.synthetic.main.home_timeline_fragment.*
+import com.thoughtworks.miniweibo.databinding.HomeTimelineFragmentBinding
 
 class HomeTimelineFragment : Fragment() {
+
+    private val viewModel: HomeTimelineViewModel by viewModels()
+
+    private lateinit var binding: HomeTimelineFragmentBinding
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.home_timeline_fragment, container, false)
-        return view
-    }
+        binding = DataBindingUtil.inflate(inflater,  R.layout.home_timeline_fragment, container, false)
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        val action = HomeTimelineFragmentDirections.actionHomeTimelineFragmentToPostDetailFragment()
-        button.setOnClickListener {
-            view -> view.findNavController().navigate(action)
+        binding.viewModel = viewModel
+
+        binding.setLifecycleOwner(this)
+
+        viewModel.score.observe(viewLifecycleOwner, Observer { newScore ->
+            binding.scoreText.text = newScore?.toString()
+        })
+
+
+        binding.addOneButton.setOnClickListener {
+            viewModel.addOne()
         }
+
+
+        // Bind button to nav
+        val action = HomeTimelineFragmentDirections.actionHomeTimelineFragmentToPostDetailFragment()
+        binding.button.setOnClickListener {
+                view -> view.findNavController().navigate(action)
+        }
+
+
+
+        return binding.root
     }
 }
--- app/src/main/java/com/thoughtworks/miniweibo/ui/home/HomeTimelineViewModel.kt
+++ app/src/main/java/com/thoughtworks/miniweibo/ui/home/HomeTimelineViewModel.kt
@@ -1,5 +1,6 @@
 package com.thoughtworks.miniweibo.ui.home
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -9,6 +10,21 @@ import retrofit2.Response
 import javax.security.auth.callback.Callback
 
 class HomeTimelineViewModel : ViewModel() {
+    private val _score = MutableLiveData<Int>()
+
+    val score : LiveData<Int>
+        get() = _score
+
+    init {
+        Log.i("HomeTimelineViewModel", "HomeTimeline ViewModel created")
+        _score.value = 0
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        Log.i("HomeTimelineViewModel", "HomeTimeline ViewModel destroyed")
+    }
+
     private val _response = MutableLiveData<String>()
 
     val response : LiveData<String>
@@ -27,4 +43,9 @@ class HomeTimelineViewModel : ViewModel() {
 
         })
     }
+
+    fun addOne() {
+        _score.value = (score.value)?.plus(1)
+    }
+
 }
